80641bfde33dd2fed5f4990ebe64124c2e1cf1eb
Stefan Vatov <stefan@thethracian.net>
2016-08-30 03:55:18 +0100

    Link to home added

--- src/js/components/header/index.jsx
+++ src/js/components/header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import UserBadge from '../profile/badge/index.jsx';
+import { Link } from 'react-router';
 
 class Header extends React.Component {
   render() {
     return (
       <div id="header" className="clearfix">
-        <div id="header-logo"></div>
+        <Link to="/home"><div id="header-logo"></div></Link>
         <div className="header-separator"></div>
       </div>
     );
